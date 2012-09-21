5dd6f5cf24adbd8781aa5faec8d0421392ddbf9c
Seth Bird <seth.a.bird@gmail.com>
2012-09-20 22:16:03 -0400

    config.comule-server.php has settings that work.

--- public_html/config.comule-server.php
+++ public_html/config.comule-server.php
@@ -1,4 +1,5 @@
 <?
+#this configuration info is setup for the taylorassassins.comule.com site.
 
 global $_CONFIG;
 $_CONFIG = array();
@@ -7,13 +8,16 @@ $_CONFIG['db_serv'] = 'mysql17.000webhost.com';
 $_CONFIG['db_user'] = 'a2852126_user';
 $_CONFIG['db_pass'] = 'Taylor=#1';
 $_CONFIG['db_database'] = 'a2852126_db';
+
+$_CONFIG['cas_server'] = 'my.taylor.edu';
+$_CONFIG['hostname'] = 'taylorassassins.comule.com';
+$_CONFIG['cas_port'] = 443;
+$_CONFIG['cas_dir'] = 'cas';
+
 $_CONFIG['email'] = 'seth.a.bird@gmail.com';
 $_CONFIG['addheaders'] = '';
-$_CONFIG['publickey'] = ;
-$_CONFIG['privatekey'] = ;
-$_CONFIG['hostname'] = ;
-$_CONFIG['cas_server'] = ;
-$_CONFIG['cas_port'] = ;
-$_CONFIG['cas_dir'] = ;
-$_CONFIG['admin_usernames'] = ;
+$_CONFIG['admin_usernames'] = 'seth_bird';
+
+$_CONFIG['publickey'] = '';
+$_CONFIG['privatekey'] = '';
 
